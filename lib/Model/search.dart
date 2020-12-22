93204f43d186861b60fbf934108eb60157a6cb01
Amar Singh <amar.vs@somaiya.edu>
2020-12-22 16:35:31 +0530

    Implemented selecting the user for meeting after search into a list along with their UIDs

--- lib/Model/search.dart
+++ lib/Model/search.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Search extends StatefulWidget {
@@ -8,10 +9,17 @@ class Search extends StatefulWidget {
 }
 
 class SearchState extends State<Search> {
+  //To declare variables
   bool _folded = true;
+  //Variable to store the search query
   String searchQuery;
+  //Variable to store tghe output from the database for a particular search query
   var queryResultSet = [];
   var tempSearchStore = [];
+  //variable to store the selected members
+  var selectedMembers = new Map();
+  var selectedNames = <String>{};
+  int totalSelected = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -113,7 +121,37 @@ class SearchState extends State<Search> {
                         itemCount: tempSearchStore.length,
                         shrinkWrap: true,
                         itemBuilder: (context, index) {
-                          return nameCard(tempSearchStore[index]["name"]);
+                          return nameCard(tempSearchStore[index]["name"],
+                              tempSearchStore[index]["uid"]);
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                        "Selected Members: $totalSelected",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white, fontSize: 22.0),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: Colors.white,
+                        boxShadow: kElevationToShadow[6],
+                      ),
+                      margin: EdgeInsets.only(top: 20.0),
+                      child: ListView.builder(
+                        itemCount: selectedMembers.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return listedMember(
+                              selectedMembers[selectedNames.elementAt(index)],
+                              selectedNames.elementAt(index));
                         },
                       ),
                     ),
@@ -127,32 +165,102 @@ class SearchState extends State<Search> {
     );
   }
 
-  Widget nameCard(String name) {
+//Widget to render the list of selected members
+  Widget listedMember(String name, String uid) {
     return Material(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          GestureDetector(
-            child: Container(
-              child: Text(
-                name,
-                style: TextStyle(
-                  fontSize: 22.0,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  color: Colors.white,
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10.0),
                 ),
-              ),
-              padding: EdgeInsets.all(10.0),
+                GestureDetector(
+                  onTap: () {
+                    //Here goes the code to delete the user from the list
+                    //Calling function to discard members from the list
+                    discardMember(uid);
+                  },
+                  child: Container(
+                    color: Colors.white,
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 0.5,
-            color: Colors.blue,
-          ),
-        ],
+            Container(
+              width: double.infinity,
+              height: 0.5,
+              color: Colors.blue,
+            ),
+          ],
+        ),
       ),
     );
   }
 
+//Widget to render each name slots which appears on entering a particular search query
+  Widget nameCard(String name, String uid) {
+    bool selected = false;
+    int keepCount = 0;
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selected = true;
+                  keepCount++;
+                });
+                if (keepCount % 2 == 0) {
+                  setState(() {
+                    selected = false;
+                  });
+                } else {
+                  //Calling function to add members to the list
+                  addMember(name, uid);
+                }
+              },
+              child: Container(
+                color: selected ? Colors.grey : Colors.white,
+                child: Text(
+                  name,
+                  style: TextStyle(
+                    fontSize: 22.0,
+                  ),
+                ),
+                padding: EdgeInsets.all(10.0),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 0.5,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+//Method to implement searching members for the meeting
   void searchUsers(String query) async {
     if (query.length == 0) {
       setState(() {
@@ -190,5 +298,28 @@ class SearchState extends State<Search> {
         }
       });
     }
+    print("map: $tempSearchStore");
+  }
+
+//Method to add member to the list
+  void addMember(String name, String uid) {
+    if (totalSelected < 4) {
+      selectedNames.add(uid);
+      selectedMembers[uid] = name;
+      totalSelected++;
+    } else {
+      Fluttertoast.showToast(msg: "Maximum number opf members reacehed!");
+    }
+    print("selects: $selectedMembers");
+  }
+
+//Method to remove member from the list
+  void discardMember(String uid) {
+    setState(() {
+      selectedNames.remove(uid);
+      selectedMembers.remove(uid);
+    });
+    totalSelected--;
+    print("selects: $selectedMembers");
   }
 }
