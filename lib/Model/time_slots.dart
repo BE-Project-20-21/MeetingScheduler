4592c3e4cf1f49611ac322f331b59f8f562ed163
Amar Singh <amar.vs@somaiya.edu>
2021-01-03 21:11:48 +0530

    Resolved Issue regarding timeslots.dart (initState) version problem; manage schedule functioning fine now.

--- lib/Model/time_slots.dart
+++ lib/Model/time_slots.dart
@@ -126,33 +126,89 @@ class _TimeSlotsState extends State<TimeSlots> {
   bool pressed = false;
   int counter = 1;
 
-  @override
-  void initState() async {
+  // @override
+  // void initState() async {
+  //   super.initState();
+  //   //Here Goes the entire code to check if schedule already submitted
+  //   // and if submitted read the schedule from the Database and reflect the schedule on the application
+  //   //Starting the progress bar for the period to gather the already submitted schedule
+
+  //   //Getting the authentication reference and getting the current user data
+  //   FirebaseAuth authSchedule = FirebaseAuth.instance;
+  //   User userSchedule = authSchedule.currentUser;
+  //   String uidSchedule = userSchedule.uid.toString();
+
+  //   //Code to check whether the schedule exists
+  //   FirebaseDatabase databaseSchedule = new FirebaseDatabase();
+  //   DatabaseReference referenceSchedule =
+  //       databaseSchedule.reference().child("schedule");
+  //   await referenceSchedule
+  //       .reference()
+  //       .child("schedule")
+  //       .child(uidSchedule)
+  //       .once()
+  //       .then((DataSnapshot dataSnapshot) {
+  //     if (dataSnapshot.value != null) {
+  //       //Code to load the existing schedule and populate the Maps
+  //       displaySchedule(uidSchedule);
+  //     }
+  //   });
+  // }
+
+   @override
+  void initState() {
     super.initState();
-    //Here Goes the entire code to check if schedule already submitted
-    // and if submitted read the schedule from the Database and reflect the schedule on the application
-    //Starting the progress bar for the period to gather the already submitted schedule
-
-    //Getting the authentication reference and getting the current user data
-    FirebaseAuth authSchedule = FirebaseAuth.instance;
-    User userSchedule = authSchedule.currentUser;
-    String uidSchedule = userSchedule.uid.toString();
-
-    //Code to check whether the schedule exists
-    FirebaseDatabase databaseSchedule = new FirebaseDatabase();
-    DatabaseReference referenceSchedule =
-        databaseSchedule.reference().child("schedule");
-    await referenceSchedule
-        .reference()
-        .child("schedule")
-        .child(uidSchedule)
-        .once()
-        .then((DataSnapshot dataSnapshot) {
-      if (dataSnapshot.value != null) {
-        //Code to load the existing schedule and populate the Maps
-        displaySchedule(uidSchedule);
+    //Setting the UI rendering parameters to not normal (pressed)
+    if (widget.day == "Sunday") {
+      if (sundayMap[widget.time] == true) {
+        setState(() {
+          pressed = true;
+          counter = counter + 1;
+        });
       }
-    });
+    } else if (widget.day == "Monday") {
+      if (mondayMap[widget.time] == true) {
+        setState(() {
+          pressed = true;
+          counter = counter + 1;
+        });
+      }
+    } else if (widget.day == "Tuesday") {
+      if (tuesdayMap[widget.time] == true) {
+        setState(() {
+          pressed = true;
+          counter = counter + 1;
+        });
+      }
+    } else if (widget.day == "Wednesday") {
+      if (wednesdayMap[widget.time] == true) {
+        setState(() {
+          pressed = true;
+          counter = counter + 1;
+        });
+      }
+    } else if (widget.day == "Thursday") {
+      if (thursdayMap[widget.time] == true) {
+        setState(() {
+          pressed = true;
+          counter = counter + 1;
+        });
+      }
+    } else if (widget.day == "Friday") {
+      if (fridayMap[widget.time] == true) {
+        setState(() {
+          pressed = true;
+          counter = counter + 1;
+        });
+      }
+    } else if (widget.day == "Saturday") {
+      if (saturdayMap[widget.time] == true) {
+        setState(() {
+          pressed = true;
+          counter = counter + 1;
+        });
+      }
+    }
   }
 
   //Method to display the pre-submitted schedule
