35a43a87f7464907351c139b95092be028c7fd7c
Amar Singh <amar.vs@somaiya.edu>
2021-02-15 01:32:21 +0530

    small tweaks on meetingcard, logic for accept reject yet to be executed

--- lib/Model/pending_cards.dart
+++ lib/Model/pending_cards.dart
@@ -79,19 +79,129 @@ class PendingCards extends StatelessWidget {
   }
 
   void showCard(BuildContext context, Map<dynamic, dynamic> temp) {
+    //Code to show the progress bar
+    // progressDialogMeetingCard = new ProgressDialog(context,
+    //     type: ProgressDialogType.Normal, isDismissible: false);
+    // progressDialogMeetingCard.style(
+    //   child: Container(
+    //     color: Colors.white,
+    //     child: CircularProgressIndicator(
+    //       valueColor: new AlwaysStoppedAnimation<Color>(Color(0xFF7B38C6)),
+    //     ),
+    //     margin: EdgeInsets.all(10.0),
+    //   ),
+    //   message: "Gathering your Meetings!",
+    //   borderRadius: 10.0,
+    //   backgroundColor: Colors.white,
+    //   elevation: 40.0,
+    //   progress: 0.0,
+    //   maxProgress: 100.0,
+    //   insetAnimCurve: Curves.easeInOut,
+    //   progressWidgetAlignment: Alignment.center,
+    //   progressTextStyle: TextStyle(color: Colors.black, fontSize: 13.0),
+    //   messageTextStyle: TextStyle(color: Colors.black, fontSize: 19.0),
+    // );
+    // progressDialogMeetingCard.show();
+
+    // progressDialogMeetingCard.hide();
+
     showAnimatedDialog(
       context: context,
       barrierDismissible: true,
       builder: (BuildContext context) {
-        //Call the widget that will call show the details about the meeting
-        return cardContent(context);
+        return Container(
+          padding: EdgeInsets.all(10.0),
+          margin: EdgeInsets.all(40.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          ),
+          child: cardContent(context, temp),
+        );
       },
-      animationType: DialogTransitionType.slideFromBottomFade,
+      animationType: DialogTransitionType.slideFromBottom,
       curve: Curves.fastOutSlowIn,
-      duration: Duration(milliseconds: 1000),
+      duration: Duration(milliseconds: 500),
     );
   }
 
   //Widget used to display the details about the meeting on popup
-  Widget cardContent(BuildContext context) {}
+  Widget cardContent(BuildContext context, Map<dynamic, dynamic> temp) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Material(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Meeting Subject: ${temp["subject"]}",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 30),
+                      child: RaisedButton(
+                        elevation: 5.0,
+                        onPressed: () {},
+                        padding: EdgeInsets.all(10.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        color: Colors.white,
+                        child: Text(
+                          "Accept",
+                          style: TextStyle(
+                              color: Colors.green[900],
+                              letterSpacing: 1,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Metropolis'),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 30),
+                      child: RaisedButton(
+                        elevation: 5.0,
+                        onPressed: () {},
+                        padding: EdgeInsets.all(10.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        color: Colors.white,
+                        child: Text(
+                          "Reject",
+                          style: TextStyle(
+                              color: Colors.red[900],
+                              letterSpacing: 1,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Metropolis'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
